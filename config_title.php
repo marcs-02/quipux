f6401e0af4cd5a92edd46fcb9019c2fe9ec12486
marcs-02 <marquitomosquera40@gmail.com>
2024-10-18 09:09:29 -0500

    config11

---
+++ config_title.php
@@ -0,0 +1,11 @@
+<?php
+$institucionSigla="institucionSigla";
+$institucionNombre="institucionNombre";
+$banner1 = "imagenes/logolisto.jpg";
+$banner2 = "imagenes/logolisto.jpg";
+$linkBanner1 = "imagenes/logolisto.jpg";
+$nombreLinkBanner1 = "imagenes/logolisto.jpg";
+$linkBanner2 = "imagenes/logolisto.jpg";
+$nombreLinkBanner2 = "imagenes/logolisto.jpg";
+$footerText = "footerText";
+?>
---
+++ html_a_pdf/config.php
@@ -0,0 +1,4 @@
+<?php
+	$nombre_servidor = "http://192.168.0.100/quipux/html_a_pdf";
+    $tipo_sistema = "Quipux-Comunidad";
+?>
