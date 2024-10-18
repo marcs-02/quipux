771fdb40458b50ba21bc318695805a5cac5bd336
marcs-02 <marquitomosquera40@gmail.com>
2024-10-18 10:42:41 -0500

    config50

--- login.php
+++ login.php
@@ -4,13 +4,9 @@
 *    Quipux    www.gestiondocumental.gov.ec
 *------------------------------------------------------------------------------
 *    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Affero General Public License as
-*    published by the Free Software Foundation, either version 3 of the
-*    License, or (at your option) any later version.
+*    it under the terms of the GNU Affero General Public License, as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 *    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 *
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see http://www.gnu.org/licenses. 
@@ -30,48 +26,102 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
         die (html_error($mensaje));
     }
 }
-    /**
-    * Verificar si el campo usuario (login) es diferente de vacio
-    **/
-    $krd = $_POST['krd'];
-    if ($krd) {
-        // Validar si el usuario y contraseña son corectos
-        include_once "$ruta_raiz/session_orfeo.php";
-        
-        require_once "$ruta_raiz/securesession.class.php";  //Esta clase maneja seguridad en sessiones
+/**
+* Verificar si el campo usuario (login) es diferente de vacio
+**/
+$krd = $_POST['krd'];
+if ($krd) {
+    // Validar si el usuario y contraseña son corectos
+    include_once "$ruta_raiz/session_orfeo.php";
+    
+    require_once "$ruta_raiz/securesession.class.php";  //Esta clase maneja seguridad en sessiones
 
-        //Cambios contra Session Fixation
-        if (!isset($_SESSION['initiated']) && isset($_SESSION["krd"]))
-        {
-          $ss = new SecureSession();
-          $ss->check_browser = true;
-          $ss->check_ip_blocks = 2;
-          $ss->secure_word = 'QUIPUX_COMUNIDAD_V4';
-          $ss->regenerate_id = false; //true;
-          $ss->Open();
-          //  session_regenerate_id();
-            $_SESSION['initiated'] = true;
-        }
-
-	// Verificar si es usuario nuevo o no en caso de ser usuario nuevo pide cambio de contraseña.
-    	if($usua_nuevo==0) {
-            include($ruta_raiz."/contraxx.php");
-            die("");
-        }
-        if (isset($_SESSION["krd"])) {
-            echo "<script>window.location = 'index_frames.php';</script>";
-            die ("");
-        }
+    //Cambios contra Session Fixation
+    if (!isset($_SESSION['initiated']) && isset($_SESSION["krd"]))
+    {
+      $ss = new SecureSession();
+      $ss->check_browser = true;
+      $ss->check_ip_blocks = 2;
+      $ss->secure_word = 'QUIPUX_COMUNIDAD_V4';
+      $ss->regenerate_id = false; //true;
+      $ss->Open();
+      $_SESSION['initiated'] = true;
     }
-    include_once "funciones_interfaz.php";
-    // En caso de bloqueo general del sistema
-    $mensaje = "Estamos experimentando dificultades t&eacute;cnicas.<br>Por favor vuelva a intentarlo m&aacute;s tarde.";
-    $mensaje .= "<br><br>Para ir a la pantalla de ingreso, haga click&nbsp<a href=\"$ruta_raiz/login.php\" target=\"_parent\" class=\"aqui\">&quot;AQUI&quot;</a>";
+
+    // Verificar si es usuario nuevo o no en caso de ser usuario nuevo pide cambio de contraseña.
+    if($usua_nuevo==0) {
+        include($ruta_raiz."/contraxx.php");
+        die("");
+    }
+    if (isset($_SESSION["krd"])) {
+        echo "<script>window.location = 'index_frames.php';</script>";
+        die ("");
+    }
+}
+include_once "funciones_interfaz.php";
+// En caso de bloqueo general del sistema
+$mensaje = "Estamos experimentando dificultades t&eacute;cnicas.<br>Por favor vuelva a intentarlo m&aacute;s tarde.";
+$mensaje .= "<br><br>Para ir a la pantalla de ingreso, haga click&nbsp<a href=\"$ruta_raiz/login.php\" target=\"_parent\" class=\"aqui\">&quot;AQUI&quot;</a>";
 //    die (html_error($mensaje));
 ?>
 <html>
     <?echo html_head(); /*Imprime el head definido para el sistema*/?>
-    <style type="text/css"> a:link, a:visited, a:hover {color: blue;} </style>
+    <style type="text/css">
+        a:link, a:visited, a:hover {color: blue;}
+        
+        body {
+            background-color: #f4f4f9; /* Color de fondo suave */
+            font-family: 'Poppins', sans-serif; /* Fuente moderna */
+        }
+
+        #wrapper {
+            margin-top: 50px; /* Espaciado superior */
+        }
+
+        h1 {
+            color: #2A66A1; /* Color del título */
+        }
+
+        h2 {
+            color: #333; /* Color del subtítulo */
+        }
+
+        table {
+            background-color: #ffffff; /* Fondo blanco para el formulario */
+            border-radius: 10px; /* Bordes redondeados */
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* Sombra ligera */
+            padding: 20px; /* Espaciado interno */
+        }
+
+        input.tex_area {
+            width: 100%; /* Full width inputs */
+            padding: 10px; /* Espaciado interno */
+            border-radius: 5px; /* Bordes redondeados */
+            border: 1px solid #ddd; /* Borde claro */
+        }
+
+        input[type="submit"], input[type="reset"] {
+            background-color: #2A66A1; /* Color de fondo de botones */
+            color: white; /* Color de texto de botones */
+            border: none; /* Sin borde */
+            border-radius: 5px; /* Bordes redondeados */
+            padding: 10px 15px; /* Espaciado interno */
+            cursor: pointer; /* Cursor pointer */
+            transition: background-color 0.3s; /* Transición suave */
+        }
+
+        input[type="submit"]:hover, input[type="reset"]:hover {
+            background-color: #1a4a7d; /* Color al pasar el ratón */
+        }
+
+        a {
+            color: #2A66A1; /* Color de los enlaces */
+        }
+
+        a:hover {
+            text-decoration: underline; /* Subrayado al pasar el ratón */
+        }
+    </style>
 
     <script type="text/javascript" src="<?=$ruta_raiz?>/js/md5.js"></script>
     <? include_once "$ruta_raiz/js/ajax.js"; ?>
@@ -93,8 +143,6 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
 
        function validar_login () {
             if (!intento_login) {
-                // Si ya se hizo submit bloquea el submit para que no se haga varias veces (teniendo presionada la tecla enter)
-                // Ataque de negacion de servicios
                 return false;
             }
             usr = trim(document.getElementById("krd").value);
@@ -115,7 +163,7 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
                 document.getElementById("drd").value = MD5(document.getElementById("drd").value);
                 document.form_login.submit();
             } else {
-                alert('Asegúrese de ingresar su usuario y contraseña.'); //\nSi es la primera vez que ingresa al sistema, su contraseña es "123"');
+                alert('Asegúrese de ingresar su usuario y contraseña.');
             }
             return flag;
         }
@@ -127,9 +175,9 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
         }
         function detectarPhone(){
             var navegador = navigator.userAgent.toLowerCase();
-            if ( navigator.userAgent.match(/iPad/i) != null)//detectar ipad
+            if ( navigator.userAgent.match(/iPad/i) != null)
               return 2;
-            else{//detectar phone        
+            else{
                 if( navegador.search(/iphone|ipod|blackberry|android/) > -1 )
                    return 1;    
                 else 
@@ -139,7 +187,7 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
         
         window.focus();
         
-        if (window.menubar.visible || window.toolbar.visible) { // si estan activas las barras llame a index para que se bloqueen
+        if (window.menubar.visible || window.toolbar.visible) {
           if (detectarPhone()==0)
             window.location="index.php";
         }
@@ -147,94 +195,85 @@ if ($activar_bloqueo_sistema and $txt_administrador != 1) {
         // -->
     </script>
 
-
     <body class="f-default light_slate" onLoad='document.getElementById("krd").focus();'>
-        <div id="wrapper">
+        <div id="wrapper" class="container">
         <? echo html_encabezado(); /*Imprime el encabezado del sistema*/ ?>
-        <div id="mainbody">
+        <div id="mainbody" class="text-center">
             <div class="shad-1">
                 <div class="shad-2">
                     <div class="shad-3">
                         <div class="shad-4">
                             <div class="shad-5">
-<table align="center" width="100%" cellpadding="0" cellspacing="0"><!-- class="mainbody"-->
-
- <tr valign="top" align="center">
-	<td class="left"  align="center" width="100%">
-	<div class="moduletable">
-        <h1>Ingreso de Usuarios al sistema</h1>
-        <hr />
-        <table cellspacing="3" cellpadding="0" border="0" align="center" width="100%">
-        <tbody>
-            <tr>
-                <td align="center" width="100%">
-                    <? echo html_validar_browser(); /*Valida el browser*/ ?>
-                </td>
-            </tr>
-            <tr >
-                <td width="100%" align="center">                    
-        <form name="form_login" action="" method="post" onSubmit="return validar_login();">
-            <table width="350" cellpadding="0" cellspacing="7">
-                <tr>
-                    <td align="center" colspan="2"><h2> Por favor Ingrese su n&uacute;mero de C&eacute;dula y contraseña </h2></td>
-                </tr>
-                <tr>
-                    <td align="center" colspan="2"> </td>
-                </tr>
-                <tr>
-                    <td align="right" width="30%">C&eacute;dula:</td>
-<!--                    <td width="70%"><input type="text" id='krd' name="krd" size="20" maxlength="15" class="tex_area" onchange="login_buscar_tipo_usuario()"></td>-->
-                    <td width="70%"><input type="text" id='krd' name="krd" size="20" maxlength="50" class="tex_area"></td>
-            	</tr>
-                <tr>
-                    <td align="right">Contrase&ntilde;a:</td>
-                    <td><input type=password name="drd" id="drd" size="20" class="tex_area"></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <div id="div_tipo_usuario"></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                        <a href="javascript:login_olvido_contraseña()">¿Olvid&oacute; su contrase&ntilde;a?</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                        <br>
-                        <input name="Submit" type="submit" class="botones" value="Ingresar">
-                        &nbsp;&nbsp;
-                        <input type="reset" value="Borrar" class="botones" name="reset">
-                    </td>
-                </tr>
-            </table>
-        </form>
-                     </td>
-                 </tr>
-            </tbody>
-            </table>
-        </div>
-        </td>
-    </tr>
-</table>
-
-
+                                <h1>Ingreso de Usuarios al sistema</h1>
+                                <hr />
+                                <div class="moduletable">
+                                    <table align="center" width="100%" cellpadding="0" cellspacing="0">
+                                        <tr valign="top" align="center">
+                                            <td class="left" align="center" width="100%">
+                                                <table cellspacing="3" cellpadding="0" border="0" align="center" width="100%">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td align="center" width="100%">
+                                                                <? echo html_validar_browser(); /*Valida el browser*/ ?>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td width="100%" align="center">                    
+                                                                <form name="form_login" action="" method="post" onSubmit="return validar_login();">
+                                                                    <table class="table">
+                                                                        <tr>
+                                                                            <td align="center" colspan="2">
+                                                                                <h2>Por favor Ingrese su n&uacute;mero de C&eacute;dula y contraseña</h2>
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td align="right">C&eacute;dula:</td>
+                                                                            <td>
+                                                                                <input type="text" id='krd' name="krd" class="tex_area" maxlength="50">
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td align="right">Contrase&ntilde;a:</td>
+                                                                            <td>
+                                                                                <input type="password" name="drd" id="drd" class="tex_area">
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td colspan="2">
+                                                                                <div id="div_tipo_usuario"></div>
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td colspan="2" align="center">
+                                                                                <a href="javascript:login_olvido_contraseña()">¿Olvid&oacute; su contrase&ntilde;a?</a>
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td colspan="2" align="center">
+                                                                                <br>
+                                                                                <input name="Submit" type="submit" class="botones" value="Ingresar">
+                                                                                &nbsp;&nbsp;
+                                                                                <input type="reset" value="Borrar" class="botones" name="reset">
+                                                                            </td>
+                                                                        </tr>
+                                                                    </table>
+                                                                </form>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                      </div>
                 </div>
              </div>
          </div>
-      </div>
-<br><br>
-
+        <br><br>
         <? echo html_pie_pagina(); /*Imprime el pie de pagina del sistema*/ ?>
         </div>
- 
-    <script type="text/javascript">
-//        if (trim(document.getElementById("krd").value) != "") {
-//            login_buscar_tipo_usuario();
-//        }
-    </script>
-</body>
+    </body>
 </html>
