ac0efa31b2a92e559ca4b2e4fd12d05dcbe0c363
marcs-02 <marquitomosquera40@gmail.com>
2024-10-19 22:27:32 -0500

    cerrarsession

--- cerrar_session.php
+++ cerrar_session.php
@@ -1,4 +1,4 @@
-<? 
+<?php
 /**  Programa para el manejo de gestion documental, oficios, memorandus, circulares, acuerdos
 *    Desarrollado y en otros Modificado por la SubSecretaría de Informática del Ecuador
 *    Quipux    www.gestiondocumental.gov.ec
