1981b48da655f56fe2a658612853ed6f2326ff74
marcs-02 <marquitomosquera40@gmail.com>
2024-10-23 15:09:28 -0500

    ff

--- Administracion/formAdministracion.php
+++ Administracion/formAdministracion.php
@@ -17,6 +17,9 @@
  * junto con este programa. Si no, consulta http://www.gnu.org/licenses. 
  *------------------------------------------------------------------------------
  **/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $ruta_raiz = "..";
 session_start();
